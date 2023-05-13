fffc9635694e44d3df56b33e1dbb933ad9c0be53
Mohan-k7 <mohankilli27@gmail.com>
2023-05-13 11:17:13 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+var nam=new Array();
+nam[0]="ravi";
+nam[1]="lat";
+nam[2]="John";
+nam[3 ]="jack";
+nam[4]="peer";
+nam[5]="max";
+nam[6]="Jaq";
+nam[7]="kid";
+nam[8]="luffy";
+nam[9]="zoro";
+for(i=0;i<nam.length;i++)
+{
+  if(nam[i].charAt(0)==='j' || nam[i].charAt(0)==='J')
+  console.log("GoodBye "+nam[i]);
+  else
+  console.log("Hello "+nam[i]);
+}
