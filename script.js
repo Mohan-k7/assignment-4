e3460164aca2a71f330285480da2e6754c04a0fb
Mohan-k7 <133000612+Mohan-k7@users.noreply.github.com>
2023-05-13 12:09:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 var nam=new Array();
-nam[0]="Ronny";
-nam[1]="lat";
-nam[2]="John";
-nam[3 ]="jack";
-nam[4]="peer";
-nam[5]="max";
-nam[6]="Jaq";
-nam[7]="kid";
-nam[8]="luffy";
-nam[9]="zoro";
+nam[0]="Yaakov";
+nam[1]="John";
+nam[2]="Jen";
+nam[3 ]="Jason";
+nam[4]="paul";
+nam[5]="Frank";
+nam[6]="Larry";
+nam[7]="paula";
+nam[8]="laura";
+nam[9]="Jim";
 for(i=0;i<nam.length;i++)
 {
   if(nam[i].charAt(0)==='j' || nam[i].charAt(0)==='J')
